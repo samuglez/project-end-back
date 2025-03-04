600f438c42a0621890709f12a9b7da6e22da5c00
Samuel Castro <samuglez90@gmail.com>
2025-03-04 19:50:57 +0000

    Conexion BD

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 require("dotenv").config();
 
 // ℹ️ Connects to the database
-require("./db");
+require("./db/index");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+require("./models/User.model")
 
 const app = express();
 
@@ -17,6 +18,9 @@ require("./config")(app);
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
